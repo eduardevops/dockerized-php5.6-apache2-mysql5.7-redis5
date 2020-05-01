b49f531d052cd9ba207ae7badf5e5226d7b26f29
Eduard Saryan <eduardevops@users.noreply.github.com>
2020-05-01 21:05:12 +0400

    Update scripts

--- backup/web_restore.sh
+++ backup/web_restore.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Getting current date
+# Get current date.
 now=$(date +"%d-%b-%Y")
 
-cd /opt/dockerized-php5.6/
 
-# Restoring web folder
+# Restoring web folder.
+cd /path/to/dockerized-php5.6/
 docker run --rm -it -v $(docker inspect --format '{{ range .Mounts }}{{ if eq .Destination "/var/www/html/stackpost" }}{{ .Name }}{{ end }}{{ end }}' $(docker-compose ps -a -q stackpost)):/stackpost -v /backup/stackpost:/backup debian:stretch-slim tar xvfz /backup/stackpost_web-$now.tar
