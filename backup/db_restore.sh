a32722c32121680d19527b32064dc45c3a7c5ec9
Eduard Saryan <eduard.saryan@protonmail.com>
2021-04-03 00:49:15 +0400

    Update paths

--- backup/db_restore.sh
+++ backup/db_restore.sh
@@ -6,4 +6,4 @@ now=$(date +"%d-%b-%Y")
 
 # Restore DB.
 source /path/to/dockerized-php5.6/.env.db
-cat /backup/${MYSQL_DATABASE}_${now}.sql | docker exec -i $(docker ps -qf name=stackpost-db) /usr/bin/mysql -u stackpost_user --default-character-set=utf8mb4 -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}
+cat /backup/${MYSQL_DATABASE}_${now}.sql | docker exec -i $(docker ps -qf name=webapp-db) /usr/bin/mysql -u webapp_user --default-character-set=utf8mb4 -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}
--- backup/web_restore.sh
+++ backup/web_restore.sh
@@ -6,4 +6,4 @@ now=$(date +"%d-%b-%Y")
 
 # Restoring web folder.
 cd /path/to/dockerized-php5.6/
-docker run --rm -it -v $(docker inspect --format '{{ range .Mounts }}{{ if eq .Destination "/var/www/html/stackpost" }}{{ .Name }}{{ end }}{{ end }}' $(docker-compose ps -a -q stackpost)):/stackpost -v /backup/stackpost:/backup debian:stretch-slim tar xvfz /backup/stackpost_web-$now.tar
+docker run --rm -it -v $(docker inspect --format '{{ range .Mounts }}{{ if eq .Destination "/var/www/html/webapp" }}{{ .Name }}{{ end }}{{ end }}' $(docker-compose ps -a -q webapp)):/webapp -v /backup/webapp:/backup debian:stretch-slim tar xvfz /backup/webapp-web-$now.tar
